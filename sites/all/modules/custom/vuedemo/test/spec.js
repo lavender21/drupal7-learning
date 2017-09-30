17160fcb6aa4a6c1443cec4d1f0464a348c12bd3
rtning <1573872488@qq.com>
2017-09-30 10:06:50 +0800

    Use jasmine in module

---
+++ sites/all/modules/custom/vuedemo/test/spec.js
@@ -0,0 +1,6 @@
+describe("Test drupal module can use jasmine or not", function () {
+    it("console log *****success******** if success", function () {
+      	console.log("*****success********")
+        expect(true).toBe(true);
+    });
+});
